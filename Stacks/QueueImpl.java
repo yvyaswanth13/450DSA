43c74daaeee93fd9b1047849c9383c3fab77a5ff
yvyaswanth13 <84392943+yvyaswanth13@users.noreply.github.com>
2025-02-04 05:49:56 +0530

    Create QueueImpl.java

---
+++ Stacks/QueueImpl.java
@@ -0,0 +1,27 @@
+class MyQueue {
+
+    int front, rear;
+	int arr[] = new int[100005];
+
+    MyQueue()
+	{
+		front=0;
+		rear=0;
+	}
+	
+	//Function to push an element x in a queue.
+	void push(int x)
+	{
+	    arr[rear++]=x;
+	} 
+
+    //Function to pop an element from queue and return that element.
+	int pop()
+	{
+	    if(front <rear)
+	    return arr[front++];
+	    else
+	    return -1;
+		// Your code here
+	} 
+}

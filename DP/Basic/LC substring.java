492504180a0b86aeb1b2e0bb8b901676beab7216
yvyaswanth13 <84392943+yvyaswanth13@users.noreply.github.com>
2025-02-08 22:53:21 +0530

    Create LC substring.java

---
+++ DP/Basic/LC substring.java
@@ -0,0 +1,19 @@
+public int LCS(String str1, String str2) {
+    int m = str1.length();
+    int n = str2.length();
+    int[][] dp = new int[m + 1][n + 1];
+    int maxLength = 0;
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                maxLength = Math.max(maxLength, dp[i][j]);
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    return maxLength; // ✅ Always returns the correct answer
+}

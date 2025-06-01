8846957f304d83ae383b2db1652e548ae041169a
yvyaswanth13 <84392943+yvyaswanth13@users.noreply.github.com>
2025-06-01 11:45:20 +0530

    Update 1.Minimize the Heights II .java

--- Arrays/Medium/1.Minimize the Heights II .java
+++ Arrays/Medium/1.Minimize the Heights II .java
@@ -2,33 +2,27 @@
 
 class Solution {
     int getMinDiff(int[] arr, int k) {
+        // code here
         Arrays.sort(arr);
-        
         int n=arr.length;
-        int ans=arr[n-1]-arr[0];
-        int min =arr[0]+k;
-        int max= arr[n-1]-k;
-       
-        if (min > max) { // Ensure min is actually smaller
-            int temp = min;
-            min = max;
-            max = temp;
-        }
-       
-       
-       
-       for(int i=1;i<n;i++)
-       {
-         int  mi=Math.min(arr[i]-k,min);
-          int ma=Math.max(arr[i-1]+k,max);
-           if(mi<0)
-           continue;
-           
-           
-           ans=Math.min(ans,ma-mi);
-           
-       }
         
+        int ans= arr[n-1]-arr[0];
+        
+        int min= arr[0]+k;
+        int max=arr[n-1]-k;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]-k<0)
+            continue;
+            // reason abv condition->After the operation, the resultant array should not contain any negative integers. 
+            
+            int mi= Math.min(min,arr[i]-k);
+            int ma= Math.max(max,arr[i-1]+k);
+            
+            ans=Math.min(ans,ma-mi);
+        }
         return ans;
+    
     }
 }
